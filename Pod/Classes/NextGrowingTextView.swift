1f987bd7afc7b134329f3c63425f3f8db26c26f5
Simon Støvring <mail@simonbs.dk>
2016-03-24 13:29:48 +0100

    Adds property alignsTextToTop which disables scrolls to bottom

--- Pod/Classes/NextGrowingTextView.swift
+++ Pod/Classes/NextGrowingTextView.swift
@@ -79,6 +79,8 @@ public class NextGrowingTextView: UIScrollView {
         }
     }
     
+    public var alignsTextToTop = false
+    
     public override init(frame: CGRect) {
         let textView = NextGrowingInternalTextView(frame: CGRect(origin: CGPoint.zero, size: frame.size))
         self.textView = textView
@@ -208,8 +210,10 @@ public class NextGrowingTextView: UIScrollView {
     }
     
     private func scrollToBottom() {
-        let offset = self.contentOffset
-        self.contentOffset = CGPoint(x: offset.x, y: self.contentSize.height - self.frame.height)
+        if !alignsTextToTop {
+            let offset = self.contentOffset
+            self.contentOffset = CGPoint(x: offset.x, y: self.contentSize.height - self.frame.height)
+        }
     }
     
     private func simulateHeight(line: Int) -> CGFloat {
